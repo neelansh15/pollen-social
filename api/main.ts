ca08acfdcd0dd079897a437d7ef72501e0f30f85
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-07-09 15:51:30 +0530

    ft: fastify and pinata api working

--- api/main.ts
+++ api/main.ts
@@ -1,3 +1,33 @@
 
-const x: number = 10
-console.log(x)
+import { PINATA_API_KEY, PINATA_API_SECRET } from './keys'
+import fetch from 'node-fetch'
+import Fastify from 'fastify'
+import { BASE_URL } from './constants'
+const fastify = Fastify()
+
+fastify.get('/', async (req, reply) => {
+    // Test Pinata Authentication
+    const result = await fetch(BASE_URL + '/data/testAuthentication', {
+        headers: {
+            'pinata_api_key': PINATA_API_KEY,
+            'pinata_secret_api_key': PINATA_API_SECRET,
+        }
+    })
+    const data = await result.json()
+    reply.send({
+        helu: 'there',
+        ...data
+    })
+})
+
+// Run the server
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 })
+        console.log("Listening on http://localhost:3000")
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
