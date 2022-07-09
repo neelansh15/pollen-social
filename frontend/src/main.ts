a5e4acd93d72c3f518495700711d96a0952e9fa4
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-07-10 04:01:03 +0530

    ft: router and store

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { router } from './router'
+import { createPinia } from 'pinia'
+
+createApp(App).use(router).use(createPinia()).mount('#app')
